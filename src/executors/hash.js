75e6f7487ddaadde6accd2cfa7ccd194e81f9379
ilia-devfamily <i.mazhugin@dev.family>
2022-12-18 02:08:23 +0300

    (chore) complete Hash calculation

--- src/executors/hash.js
+++ src/executors/hash.js
@@ -1,2 +1,14 @@
-const calcHash = () => {};
+import crypto from "crypto";
+import { join } from "path";
+import fs from "fs/promises";
+
+const calcHash = async data => {
+  const [file] = data;
+  const path = join(process.cwd(), file);
+
+  const dataToHash = await fs.readFile(path, { encoding: "utf-8" });
+  const hash = crypto.createHash("sha256").update(dataToHash);
+
+  console.log(hash.digest("hex"));
+};
 export { calcHash };
